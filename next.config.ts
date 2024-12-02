3d3029a17d235b043fd7f27ae9756be22ebe4312
gornvan <grobelvan@gmail.com>
2024-12-02 18:30:00 +0100

    next.config.ts: redirect from / to /pages/home

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pages/home',
+        permanent: true,  // This will make it a permanent redirect (HTTP 301)
+      },
+    ];
+  },
 };
+
+
 
 export default nextConfig;
