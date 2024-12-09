a42249d7e1e6dfc929a8c9c30d826a9f91dfc41f
gornvan <grobelvan@gmail.com>
2024-12-09 19:33:55 +0100

    extend the next-auth's Session interface to inclue the accessToken in it.

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    user: {
+      id?: string;
+      name?: string;
+      email?: string;
+      image?: string;
+    };
+  }
+
+  interface JWT {
+    accessToken?: string;
+    id?: string;
+  }
+}
