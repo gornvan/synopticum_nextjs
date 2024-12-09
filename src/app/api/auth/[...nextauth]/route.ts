9aa7d1b375d7ad705a2c16e4efe8f16337465ff4
gornvan <grobelvan@gmail.com>
2024-12-09 19:34:55 +0100

    Put AccessToken into Session by capturing it during jwt callback and setting into Session in session callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions, JWT } from "next-auth"
 import KeycloakProvider from "next-auth/providers/keycloak";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     KeycloakProvider({
@@ -11,6 +11,25 @@ export const authOptions = {
     })
     //..add more providers here
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // If account is available, it's the first sign-in
+      if (account) {
+        token.accessToken = account.access_token; // Store the original access_token
+        token.idToken = account.id_token; // Optionally store the id_token
+      }
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token and user id from a provider.
+      const jwtoken = token as JWT;
+      session.accessToken = jwtoken.accessToken
+      session.user.id = jwtoken.id
+
+      return session
+    }
+  }
 }
 
 const handler = NextAuth(authOptions);
