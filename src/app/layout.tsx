d42ff1b2cd9133ef9e8a1b162ee0b88fbbc197b2
gornvan <grobelvan@gmail.com>
2024-12-18 18:05:27 +0100

    use the new client provider on the Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import SessionProviderWrapper from "./account/SessionProviderWrapper";
 
 import { Navigation } from '@/app/layout/navigation';
+import { SynopticumApiClientProviderWrapper } from "./apiClients/synopticum/SynopticumApiClientProviderWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,8 +32,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <SessionProviderWrapper>
+            <SynopticumApiClientProviderWrapper>
             <Navigation/>
             {children}
+            </SynopticumApiClientProviderWrapper>
           </SessionProviderWrapper>
       </body>
     </html>
