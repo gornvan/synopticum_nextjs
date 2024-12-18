e971778dc5199c17ec47e6f35d2f51c794033f0e
gornvan <grobelvan@gmail.com>
2024-12-18 18:06:56 +0100

    use the Grid Component on the Forecasts page instead of having its implementation directly on the page

--- src/app/pages/forecasts/page.tsx
+++ src/app/pages/forecasts/page.tsx
@@ -1,16 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
 import { Container, Typography, CircularProgress, Alert } from "@mui/material";
-import { useSession } from "next-auth/react"
 import { Forecast, SynopticumApiClient, WeatherSummary }
   from "@/app/apiClients/synopticum/SynopticumApiClient";
-import GenericGrid from "@/app/components/genericGrid/GenericGrid";
-
-const ForecastsGridPage = () => {
-  const [forecasts, setForecasts] = useState<Forecast[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-
+import ForecastsGrid from "./ForecastsGrid";
 
 // Creating a forecast
 async function postForecast(client: SynopticumApiClient) {
@@ -27,54 +19,12 @@ async function postForecast(client: SynopticumApiClient) {
   }
 }
 
-const { data: sessionData, status: sessionStatus } = useSession();
+const ForecastsGridPage = () => {
+  return <Container>
+    
+    <ForecastsGrid></ForecastsGrid>
+  </Container>
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      
-      const client = new SynopticumApiClient(sessionData?.accessToken ?? "");
-      
-      if(sessionStatus == 'authenticated'){
-        postForecast(client);
-      }
-
-      try {
-        const data = await client.fetchForecasts("Belarus", "Minsk", {
-          minTemperatureC: 1,
-          maxTemperatureC: 20,
-          pageSize: 50,
-          pageNumber: 1,
-        });
-        setForecasts(data);
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  // Enum mapping for the grid
-  const enumMap = {
-    summary: WeatherSummary,
-  };
-
-  return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Weather Forecasts
-      </Typography>
-      {loading && <CircularProgress />}
-      {error && (
-        <Alert severity="error" style={{ marginBottom: "16px" }}>
-          Error: {error}
-        </Alert>
-      )}
-      {!loading && !error && <GenericGrid data={forecasts} enumMap={enumMap} />}
-    </Container>
-  );
-};
 
 export default ForecastsGridPage;
